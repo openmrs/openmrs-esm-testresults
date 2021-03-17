fe66b0e144af55f31623e903a8ea58d0c45db7a3
Nikita Malyschkin <nikita.malyschkin@googlemail.com>
2021-03-17 11:12:07 +0100

    add timeline interpretations

--- src/timeline/timeline.component.tsx
+++ src/timeline/timeline.component.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import { OBSERVATION_INTERPRETATION } from '../loadPatientTestData/helpers';
 import styles from './timeline.scss';
 import useScrollIndicator from './useScroll';
 import { useTimelineData } from './useTimelineData';
@@ -21,16 +22,65 @@ const PaddingContainer = React.forwardRef((props, ref) => (
 ));
 const TimeSlotsInner = props => <div className={styles['time-slot-inner']} {...props} />;
 const Main = ({ className = '', ...props }) => <main {...props} className={`omrs-main-content ${className}`} />;
-const TimelineCell = ({ text }) => (
-  <div className={styles['timeline-cell']}>
-    <p>{text}</p>
-  </div>
-);
+
+const TimelineCell = ({ text, interpretation = OBSERVATION_INTERPRETATION.NORMAL }) => {
+  switch (interpretation) {
+    case OBSERVATION_INTERPRETATION.OFF_SCALE_HIGH:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['off-scale-high']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.CRITICALLY_HIGH:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['critically-high']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.HIGH:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['high']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.OFF_SCALE_LOW:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['off-scale-low']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.CRITICALLY_LOW:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['critically-low']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.LOW:
+      return (
+        <div className={`${styles['timeline-cell']} ${styles['low']}`}>
+          <p>{text}</p>
+        </div>
+      );
+
+    case OBSERVATION_INTERPRETATION.NORMAL:
+    default:
+      return (
+        <div className={`${styles['timeline-cell']}`}>
+          <p>{text}</p>
+        </div>
+      );
+  }
+};
 
 const RowStartCell = ({ title, range, unit, shadow = false }) => (
   <div
     className={styles['timeline-cell']}
-    style={{ position: 'sticky', left: '0px', boxShadow: shadow ? '5px 0 5px 0 rgb(0 0 0 / 11%)' : undefined }}>
+    style={{ position: 'sticky', left: '0px', boxShadow: shadow ? '8px 0 20px 0 rgba(0,0,0,0.15)' : undefined }}>
     <p>
       {title}
       <br></br>
@@ -45,6 +95,16 @@ const TimeSlots = ({ children = undefined, style }) => (
   </TimeSlotsInner>
 );
 
+const GridItems = React.memo(({ sortedTimes, obs }) => (
+  <>
+    {sortedTimes.map((_, i) => {
+      if (!obs[i]) return <TimelineCell text={'--'} />;
+      const interpretation: OBSERVATION_INTERPRETATION = obs[i].meta.assessValue(obs[i].value);
+      return <TimelineCell text={obs[i].value} interpretation={interpretation} />;
+    })}
+  </>
+));
+
 const Table = () => {
   let { patientUuid, panelUuid } = useParams<{ patientUuid: string; panelUuid: string }>();
   const [xIsScrolled, yIsScrolled, containerRef] = useScrollIndicator(0, 32);
@@ -57,6 +117,8 @@ const Table = () => {
     },
     loaded,
   } = useTimelineData(patientUuid, panelUuid);
+
+  console.log({ rowData });
 
   if (!loaded)
     return (
@@ -84,7 +146,7 @@ const Table = () => {
           position: 'sticky',
           top: '-32px',
           zIndex: 2,
-          boxShadow: yIsScrolled ? '5px 0 5px 0 rgb(0 0 0 / 11%)' : undefined,
+          boxShadow: yIsScrolled ? '8px 0 20px 0 rgba(0,0,0,0.15)' : undefined,
         }}>
         {yearColumns.map(({ year, size }) => {
           return (
@@ -116,9 +178,7 @@ const Table = () => {
           return (
             <>
               <RowStartCell {...{ unit, range, title, shadow: xIsScrolled }} />
-              {sortedTimes.map((_, i) => (
-                <TimelineCell text={obs[i]?.value || '--'} />
-              ))}
+              <GridItems {...{ sortedTimes, obs }} />
             </>
           );
         })}
