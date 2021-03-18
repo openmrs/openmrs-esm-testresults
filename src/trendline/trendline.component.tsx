e7f5eacce0602e58246b1db6453faa627cbbd6a5
ayush.sharma <ayush.sharma@thoughtworks.com>
2021-03-18 23:48:00 +0530

    #MF-418 Aashu, Ayush: add data trendline (line-chart)

--- src/trendline/trendline.component.tsx
+++ src/trendline/trendline.component.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import usePatientResultsData from '../loadPatientTestData/usePatientResultsData';
-import styles from './timeline.scss';
+import styles from './trendline.scss';
+import { LineChart } from '@carbon/charts-react';
 
 const useTrendlineData = () => {
   const { patientUuid, panelUuid, testUuid } = useParams<{
@@ -29,11 +30,69 @@ const useTrendlineData = () => {
 };
 
 const Main = ({ className = '', ...props }) => <main {...props} className={`omrs-main-content ${className}`} />;
+const TrendLineBackground = ({ ...props }) => <div {...props} className={styles['Background']} />;
 
 const Trendline = () => {
-  //   const {} = useTrendlineData();
+  const patientData = useTrendlineData();
 
-  return <Main>Trendline goes here.</Main>;
+  if (patientData !== null) {
+    let leftAxisLabel = '';
+    const data = [];
+
+    const DateFormatOption: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric' };
+
+    // console.log(patientData)
+    let dataset = patientData[0];
+    patientData[1].entries.forEach(entry => {
+      data.push({
+        date: new Date(Date.parse(entry.effectiveDateTime)).toLocaleDateString('en-GB', DateFormatOption),
+        value: entry.value,
+        group: dataset,
+        id: entry.id,
+      });
+    });
+
+    if (patientData[1].entries[0]) {
+      leftAxisLabel = patientData[1].entries[0].meta.units;
+    }
+
+    const options = {
+      // "title": dataset,
+      axes: {
+        bottom: {
+          title: 'Date',
+          mapsTo: 'date',
+          scaleType: 'labels',
+        },
+        left: {
+          mapsTo: 'value',
+          title: leftAxisLabel,
+          scaleType: 'linear',
+        },
+      },
+      height: '20.125rem',
+
+      color: {
+        scale: {
+          dataset: '#6929c4',
+        },
+      },
+    };
+
+    return (
+      <Main>
+        <TrendLineBackground>
+          <LineChart class={styles['chartHolder']} data={data} options={options}></LineChart>
+        </TrendLineBackground>
+      </Main>
+    );
+  }
+
+  return (
+    <Main>
+      <div>Loading...</div>
+    </Main>
+  );
 };
 
 export default React.memo(Trendline);
