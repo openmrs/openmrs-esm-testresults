96caea3d9905c46542a2635f3eb16e0b6f5720aa
ayush.sharma <ayush.sharma@thoughtworks.com>
2021-03-18 23:58:00 +0530

    Co-authored-by: Aashu Rani <Aashu23>

--- src/trendline/trendline.component.tsx
+++ src/trendline/trendline.component.tsx
@@ -5,6 +5,16 @@ import usePatientResultsData from '../loadPatientTestData/usePatientResultsData'
 import styles from './trendline.scss';
 import { LineChart } from '@carbon/charts-react';
 import { ArrowLeft24 } from '@carbon/icons-react';
+import {
+  DataTable,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+} from 'carbon-components-react';
 
 import '@carbon/charts/styles.css';
 
@@ -43,14 +53,19 @@ const TrendLineBackground = ({ ...props }) => <div {...props} className={styles[
 
 const Trendline = () => {
   const patientData = useTrendlineData();
+  const history = useHistory();
+
 
   if (patientData !== null) {
     let leftAxisLabel = '';
+    let tableHeaderData = [];
+
     const data = [];
+    const tableData = [];
 
     const DateFormatOption: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric' };
-    const history = useHistory();
-
+    const TableDateFormatOption: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    const TableTimeFormatOption: Intl.DateTimeFormatOptions = { hour: '2-digit', minute: '2-digit' };
 
     // console.log(patientData)
     let dataset = patientData[0];
@@ -59,6 +74,13 @@ const Trendline = () => {
         date: new Date(Date.parse(entry.effectiveDateTime)).toLocaleDateString('en-GB', DateFormatOption),
         value: entry.value,
         group: dataset,
+        id: entry.id,
+      });
+
+      tableData.push({
+        date: new Date(Date.parse(entry.effectiveDateTime)).toLocaleDateString('en-GB', TableDateFormatOption),
+        time: new Date(Date.parse(entry.effectiveDateTime)).toLocaleTimeString('en-GB', TableTimeFormatOption),
+        value: entry.value,
         id: entry.id,
       });
     });
@@ -90,17 +112,34 @@ const Trendline = () => {
       },
     };
 
+    tableHeaderData = [
+      {
+        header: 'Date',
+        key: 'date',
+      },
+      {
+        header: 'Time',
+        key: 'time',
+      },
+      {
+        header: `Value (${leftAxisLabel})`,
+        key: 'value',
+      },
+    ];
+
+
     return (
       <Main>
-      <TrendLineHeader>
-        <>
-          <ArrowLeft24 onClick={() => history.push(`/lab-results/`)}></ArrowLeft24>
-          <div className={styles['title']}>{dataset}</div>
-        </>
-      </TrendLineHeader>
+        <TrendLineHeader>
+          <>
+            <ArrowLeft24 onClick={() => history.push(`/lab-results/`)}></ArrowLeft24>
+            <div className={styles['title']}>{dataset}</div>
+          </>
+        </TrendLineHeader>
         <TrendLineBackground>
           <LineChart class={styles['chartHolder']} data={data} options={options}></LineChart>
         </TrendLineBackground>
+        {DrawTable(tableData, tableHeaderData)}
       </Main>
     );
   }
@@ -109,6 +148,35 @@ const Trendline = () => {
     <Main>
       <div>Loading...</div>
     </Main>
+  );
+};
+
+const DrawTable = (rowData, headerData) => {
+  return (
+    <DataTable rows={rowData} headers={headerData}>
+      {({ rows, headers, getHeaderProps, getTableProps }) => (
+        <TableContainer title="DataTable">
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                {headers.map(header => (
+                  <TableHeader {...getHeaderProps({ header })}>{header.header}</TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  {row.cells.map(cell => (
+                    <TableCell key={cell.id}>{cell.value}</TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </DataTable>
   );
 };
 
